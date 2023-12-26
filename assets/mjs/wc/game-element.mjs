f63528e263867e5e5882635165ac95f266b37658
Shaun O’Connell <ndorfin+github@gmail.com>
2023-12-26 20:35:58 +1300

    Start the gameLoop

--- assets/mjs/wc/game-element.mjs
+++ assets/mjs/wc/game-element.mjs
@@ -1,5 +1,15 @@
 export default class GameElement extends HTMLElement {
+
     constructor() {
+        // Always call super first in constructor
         super();
+    }
+
+    connectedCallback() {
+        console.log(`Custom element ${this.constructor.name} added to page.`);
+    }
+
+    disconnectedCallback() {
+        console.log(`Custom element ${this.constructor.name} removed from page.`);
     }
 }
--- assets/mjs/wc/game-scene.mjs
+++ assets/mjs/wc/game-scene.mjs
@@ -2,5 +2,28 @@ import GameElement from './game-element.mjs';
 
 export default class GameScene extends GameElement {
 
+    #gameRunning = false;
+    #gameEventFrequency = 10; // milliseconds
+
+    #gameLoop = function() {
+        this.#gameEventFrequency++;
+        console.log('this.#gameEventFrequency', this.#gameEventFrequency);
+    }
+
+    startGame() {
+        this.#gameRunning = true;
+        window.setInterval(this.#gameLoop.bind(this), this.#gameEventFrequency);
+    }
+
+    stopGame() {
+        this.#gameRunning = false;
+        window.clearInterval(this.#gameLoop);
+    }
+
+    connectedCallback() {
+        this.startGame();
+    }
+  
+
 }
 

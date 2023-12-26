7d4d009a5cbfb4db0afeb0fdc54b10b11de8b2cf
Shaun O’Connell <ndorfin+github@gmail.com>
2023-12-26 20:02:08 +1300

    Import WCs in a topographical order

--- assets/mjs/index.mjs
+++ assets/mjs/index.mjs
@@ -1,13 +1,13 @@
-import GameControls from './wc/game-controls.mjs?date=2023-12-26';
 import GameScene from './wc/game-scene.mjs?date=2023-12-26';
+import GameControls from './wc/game-controls.mjs?date=2023-12-26';
 import LanderVehicle from './wc/lander-vehicle.mjs?date=2023-12-26';
 
 const customElementMappings = {
-    'game-controls': GameControls,
     'game-scene': GameScene,
+    'game-controls': GameControls,
     'lander-vehicle': LanderVehicle,       
 }
 
 for (const [customElementName, customElementClass] of Object.entries(customElementMappings)) {
-    customElements.define(customElementName, customElementClass);
+    window.customElements.define(customElementName, customElementClass);
 }
